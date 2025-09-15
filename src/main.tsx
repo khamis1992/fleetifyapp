f9b7b66ac8f6d8e2dd16b76ffe8b8d508b5fa509
khamis1992 <khamis-1992@hotmail.com>
2025-09-15 20:19:28 +0300

    555

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,21 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error('Root element not found');
 }
 
-createRoot(rootElement).render(<App />);
+const root = createRoot(rootElement);
+
+// تمكين React StrictMode في بيئة التطوير
+if (import.meta.env.DEV) {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  root.render(<App />);
+}
